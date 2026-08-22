b97934e8c7eadd5e74431054f599b4068f98576b
jjanevski83 <jjanevski83@gmail.com>
2026-08-22 07:47:48 +0200

    add T4

--- components/wr3223/sensor.py
+++ components/wr3223/sensor.py
@@ -33,6 +33,7 @@ SENSOR_COMMANDS = {
     # Drehzahl-Sensoren
     "NA": ("Drehzahl Abluft", "rpm", DEVICE_CLASS_FREQUENCY),
     "NZ": ("Drehzahl Zuluft", "rpm", DEVICE_CLASS_FREQUENCY),
+    "T4": ("Raumtemperatur", UNIT_CELSIUS, DEVICE_CLASS_TEMPERATURE),
 }
 
 CONF_COMMAND = "command"
